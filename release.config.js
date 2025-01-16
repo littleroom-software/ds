b3345801712a0034cdebea11d485fc70742a2c2c
Renan Arques <renan.arques@gmail.com>
2025-01-16 16:58:25 -0300

    ci: improve jobs

--- release.config.js
+++ release.config.js
@@ -35,17 +35,17 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     [
-      "@semantic-release/npm",
+      "@semantic-release/exec",
       {
-        npmPublish: true,
-        pkgRoot: ".",
+        prepareCmd: "npm run build --workspace=@littleroom-ui/react && npm run build --workspace=@littleroom-ui/tokens",
+        publishCmd: "npm publish --workspace=@littleroom-ui/react --workspace=@littleroom-ui/tokens",
       },
     ],
     "@semantic-release/github",
     [
       "@semantic-release/git",
       {
-        assets: ["package.json", "CHANGELOG.md"],
+        assets: ["package.json", "CHANGELOG.md", "packages/*/package.json", "packages/*/CHANGELOG.md"],
         message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
